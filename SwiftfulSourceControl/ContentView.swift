4f9807bc45623b9dd2144d9943baa200879dc1a3
anjitang <anjitang@naver.com>
2025-01-11 16:00:33 +0900

    Another commit

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,10 +15,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("jihoon heart")
-            
-            Button("Click me") {
-                
-            }
+
         }
         .padding()
     }
