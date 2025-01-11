d803595cddbe7b27eb956632b3e8887831e2143f
anjitang <anjitang@naver.com>
2025-01-11 19:01:00 +0900

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -5,6 +5,25 @@
 //  Created by jihoon an on 1/11/25.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
+
 import SwiftUI
 
 struct HomeView: View {
