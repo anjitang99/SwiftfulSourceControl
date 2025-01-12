d0e3612f5a4876852db19ae8ecf9a1ee23dc0d22
anjitang <anjitang@naver.com>
2025-01-12 15:08:32 +0900

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by jihoon an on 1/12/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("jihoon")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
